c4e50fb3c2e917e800406c8eaa83236de11a5baa
127AGomma <127agomma@frhsd.com>
2025-09-11 08:55:08 -0400

    Added userinput for yes and no to allow a user to choose whether they want to go back to main menu or not.

--- src/LogExercise.java
+++ src/LogExercise.java
@@ -3,10 +3,15 @@ import java.util.Scanner;
 public class LogExercise {
     public static void log() {
         ExerciseBot.clear();
-        System.out.println("Return home? ");
+        System.out.print("Return home? (y/n) ");
         Scanner scanner = new Scanner(System.in);
-        scanner.nextLine();
-        ExerciseBot.main(null);
+        String userInput = scanner.nextLine().toLowerCase();
+        if (userInput.equals("y") || userInput.equals("yes")) {
+            ExerciseBot.main(null);
+        }
+        else {
+            log();
+        }
 
     }
 }
