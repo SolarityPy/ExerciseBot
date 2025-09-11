54a619d1c708ed543ffb0295b2ef122e49d5346d
Karl Arabit <127karabit@frhsd.com>
2025-09-11 09:17:57 -0400

    Added an option to return home in view logs

--- src/ViewLogs.java
+++ src/ViewLogs.java
@@ -5,5 +5,14 @@ public class ViewLogs {
         for (String exercise : exerciseLogs.keySet()) {
             System.out.println(exercise + ": " + exerciseLogs.get(exercise));
         }
+        System.out.print("Return home? (y/n) ");
+        String userInput = scanner.nextLine();
+
+        if (userInput.equals("y") || userInput.equals("yes")) {
+            ExerciseBot.main(null);
+        }
+        else {
+            view(map);
+        }
     }
 }
