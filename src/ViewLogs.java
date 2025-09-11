7cd06721ab94fa384542700649c9cffbdd343357
127AAgnihotri <127aagnihotri@frhsd.com>
2025-09-11 09:14:13 -0400

    Created view

--- src/ViewLogs.java
+++ src/ViewLogs.java
@@ -1,2 +1,9 @@
+import java.util.LinkedHashMap;
+
 public class ViewLogs {
+    public static void view(LinkedHashMap<String, Integer> exerciseLogs) {
+        for (String exercise : exerciseLogs.keySet()) {
+            System.out.println(exercise + ": " + exerciseLogs.get(exercise));
+        }
+    }
 }
