f2821b63a7d781a5618f987919dc144262ef6a6d
127AGomma <127agomma@frhsd.com>
2025-09-11 09:12:16 -0400

    fixed bugs with line 17 in logexercise.java

--- src/LogExercise.java
+++ src/LogExercise.java
@@ -3,18 +3,19 @@ import java.util.Scanner;
 
 public class LogExercise {
     public static void log(LinkedHashMap<String, Integer> map) {
-        Scanner scanner = new Scanner(System.in);
         ExerciseBot.clear();
+        Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter exercise name: ");
         String exercise = scanner.nextLine();
         System.out.print("Enter number of reps: ");
         int reps = scanner.nextInt();
-
+        scanner.nextLine();
         map.put(exercise, reps);
 
         System.out.print("Return home? (y/n) ");
-        String userInput = scanner.nextLine().toLowerCase();
+        String userInput = scanner.nextLine();
+
         if (userInput.equals("y") || userInput.equals("yes")) {
             ExerciseBot.main(null);
         }
